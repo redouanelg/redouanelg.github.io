60ad6d5387a6d98d9574f4ab95695089dfa0398c
Ajit Kumar <ajit.kumar@snu.edu.in>
2017-08-16 16:34:01 +0530

    few changes towards modifying MAT431 display

--- MathJax/config/local/local.js
+++ MathJax/config/local/local.js
@@ -26,6 +26,46 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
+MathJax.Hub.Config({
+  // Only one of the two following lines, depending on user settings
+  // First allows browser-native MathML display, second forces HTML/CSS
+  //  config: ["MMLorHTML.js"], jax: ["input/TeX"],
+    
+  jax: ["input/TeX", "output/HTML-CSS"],
+  extensions: ["tex2jax.js","TeX/AMSmath.js","TeX/AMSsymbols.js",
+               "TeX/noUndefined.js"],
+  tex2jax: {
+    inlineMath: [ ["\\(","\\)"] ],
+    displayMath: [ ['$$','$$'], ['\\[','\\]'], ["\\begin{displaymath}","\\end{displaymath}"],["\\begin{align*}","\\end{align*}"] ],
+    skipTags: ["script","noscript","style","textarea","pre","code"],
+    ignoreClass: "tex2jax_ignore",
+    processEscapes: false,
+    processEnvironments: true,
+    preview: "TeX"
+  },
+  showProcessingMessages: true,
+  displayAlign: "center",
+  displayIndent: "2em",
+
+  "HTML-CSS": {
+    scale: 100,
+    availableFonts: ["STIX","TeX"],
+    preferredFont: "TeX",
+    webFont: "TeX",
+    imageFont: "TeX",
+    showMathMenu: true,
+  },
+  MMLorHTML: {
+    prefer: {
+      MSIE:    "MML",
+      Firefox: "MML",
+      Opera:   "HTML",
+      other:   "HTML"
+    }
+  }
+});
+
+
 
 
 MathJax.Hub.Register.StartupHook("TeX Jax Ready",function () {
@@ -52,5 +92,9 @@ TEX.Macro("vp","{\\varphi}");
 TEX.Macro("ve","{\\mathbf{\\varepsilon\}}");
 TEX.Macro("divc","{\\mathbf\{div\}}");
 });
+
+
+
+
 
 MathJax.Ajax.loadComplete("[MathJax]/config/local/local.js");
